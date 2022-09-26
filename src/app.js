6316e3ceffda9d0437f1ed673d11703773b47ad7
Javier Murillo Romero <javim552@gmail.com>
2022-09-26 18:40:19 +0000

    realizar ejercicio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["the dog", "my granma", "his turtle", "my bird"];
+let what = ["eat ", "pissed", "crushed", "broked"];
+let when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+let palabra = "my homework";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let primerapalabra = who[Math.floor(Math.random() * 4)];
+  let segundapalabra = what[Math.floor(Math.random() * 4)];
+  let frase = when[Math.floor(Math.random() * 5)];
+
+  let frasecompleta =
+    primerapalabra + " " + segundapalabra + " " + palabra + " " + frase;
+  console.log(frasecompleta);
+
+  document.getElementById("excuse").innerHTML = frasecompleta;
 };
